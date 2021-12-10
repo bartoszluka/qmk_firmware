a62f2de1e0c2090805a0525414a0659aca40a36e
Bartek <bartoszluka1@gmail.com>
2021-12-10 14:06:43 +0100

    Change right alt to one shot

--- keyboards/splitkb/kyria/keymaps/bartoszluka/keymap.c
+++ keyboards/splitkb/kyria/keymaps/bartoszluka/keymap.c
@@ -18,16 +18,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //# │ LSHFT │   Z   ╰───────╯       ╰───────╯       │ │ LCTRL ╰───────╮    ╭───────╯ LSHFT │ │       ╰───────╯       ╰───────╯  / ?  │ RCTRL │
     //# │       │       │              ╭───────╮╰───────╯ │       │ LWIN  │    │       │       │ ╰───────╯╭───────╮              │       │       │
     //# ╰───────╯───────╯     ╭───────╮│ LWIN  ╰───────╮  ╰───────╯       │    │       ╰───────╯  ╭───────╯ RWIN  │╭───────╮     ╰───────╰───────╯
-    //#                       │  DEL  ││       │ A/TAB ╰───────╮  ╰───────╯    ╰───────╯  ╭───────╯ A/TAB │       ││ RSHFT │
+    //#                       │  DEL  ││       │ A/TAB ╰───────╮  ╰───────╯    ╰───────╯  ╭───────╯ R ALT │       ││ RSHFT │
     //#                       │       │╰───────╯       │ S/BSC ╰───────╮          ╭───────╯ 1/SPC │       ╰───────╯│       │
     //#                       ╰───────╯        ╰───────╯       │ C/ESC │          │ 2/ENT │       ╰───────╯        ╰───────╯
     //#                                                ╰───────╯       │          │       ╰───────╯
     //#                                                        ╰───────╯          ╰───────╯
     [_QWERTY] = LAYOUT(
-KC_TAB,               KC_Q, KC_W, KC_E,   KC_R,    KC_T,                                                                                                              KC_Y,                 KC_U,    KC_I,    KC_O,   KC_P,                  KC_BSLS,
-MT(MOD_LSFT, KC_ESC), KC_A, KC_S, KC_D,   KC_F,    KC_G,                                                                                                              KC_H,                 KC_J,    KC_K,    KC_L,   MT(MOD_RCTL, KC_SCLN), KC_QUOT,
-KC_LSFT,              KC_Z, KC_X, KC_C,   KC_V,    KC_B,                 KC_LCTL,               KC_LGUI,                _______,                KC_LSFT,              KC_N,                 KC_M,    KC_COMM, KC_DOT, KC_SLSH,               KC_RCTL,
-                                  KC_DEL, KC_LGUI, MT(MOD_LALT, KC_TAB), MT(MOD_LSFT, KC_BSPC), MT(MOD_LCTL, KC_ESC),   LT(_FANDMEDIA, KC_ENT), LT(_SYMBOLS, KC_SPC), MT(MOD_RALT, KC_TAB), KC_RGUI, KC_RSFT
+KC_TAB,               KC_Q, KC_W, KC_E,   KC_R,    KC_T,                                                                                                              KC_Y,          KC_U,    KC_I,    KC_O,   KC_P,                  KC_BSLS,
+MT(MOD_LSFT, KC_ESC), KC_A, KC_S, KC_D,   KC_F,    KC_G,                                                                                                              KC_H,          KC_J,    KC_K,    KC_L,   MT(MOD_RCTL, KC_SCLN), KC_QUOT,
+KC_LSFT,              KC_Z, KC_X, KC_C,   KC_V,    KC_B,                 KC_LCTL,               KC_LGUI,                _______,                KC_LSFT,              KC_N,          KC_M,    KC_COMM, KC_DOT, KC_SLSH,               KC_RCTL,
+                                  KC_DEL, KC_LGUI, MT(MOD_LALT, KC_TAB), MT(MOD_LSFT, KC_BSPC), MT(MOD_LCTL, KC_ESC),   LT(_FANDMEDIA, KC_ENT), LT(_SYMBOLS, KC_SPC), OSM(MOD_RALT), KC_RGUI, KC_RSFT
       ),
 
     //#                         ╭───────╮                                                                        ╭───────╮
@@ -261,6 +261,7 @@ qmk-keyboard-format:json:begin
         ,"KC_RSFT": "RSHFT"
         ,"KC_LCTL": "LCTRL"
         ,"KC_RCTL": "RCTRL"
+        ,"OSM(MOD_RALT)": "R ALT"
         ,"KC_LALT": "LALT "
         ,"KC_RALT": "RALT "
         ,"KC_LGUI": "LWIN "
